5f5329373d2357e006bcbb76631492b912f87295
VIMRG <98758003+VIMRG@users.noreply.github.com>
2023-08-14 11:59:14 -0500

    Add files via upload

--- Scripts/HIVnegative_Analyses/Dialogue_HIVneg.R
+++ Scripts/HIVnegative_Analyses/Dialogue_HIVneg.R
@@ -1,6 +1,6 @@
 ##############################################################################################
 ##------------------------- DIALOGUE ---------------------------------##
-##------------------------- DATE: 6/27/2022 AUTHOR: SAM BAILIN-------------------------------##
+##------------------------- AUTHOR: SAM BAILIN-------------------------------##
 ## DESCRIPTION: This script will use Dialogue to discover gene regulatory programs across 
 # cell types that vary with disease state. 
 ##############################################################################################
@@ -30,10 +30,9 @@ options(future.seed = TRUE)
 set.seed(7412)
 
 date = "6.27"
-tmp <- paste0("/data/p_koethe_lab/Atlas_AT/HATIM_Analysis/", date)
-fig_dir <- "/data/p_koethe_lab/Atlas_AT/HATIM_Analysis/6.27/Figures/"
+tmp <- paste0("../", date)
 
-data_hatim <- readr::read_csv('/data/p_koethe_lab/Atlas_AT/MetaData/Reference/HATIMStudyVisit_DATA_2021-08-09_1359.csv')
+data_hatim <- readr::read_csv('../HATIMStudyVisit_DATA_2021-08-09_1359.csv')
 data_hatim <- data_hatim %>% mutate(HATIMID = as.factor(hatim_clin_visit_pid),
                                     StudyGroup = factor(hatim_final_arm, levels = c(1,2,3,4),
                                                         labels = c("HIV+ non-diabetic", "HIV+ prediabetic", 
@@ -72,7 +71,7 @@ Stromal[['GlucoseIntolerant']] <- ifelse(Stromal$StudyGroup == "HIV+ non-diabeti
 # the seurat objects and extract the necessary information. To limit to many 
 # interactions, I will subset on prespecified cells types that have either shown proportional
 # differences or have biological relevance to T2DM. This will include the following:
-# T cells: CD4 TEM, CD8 TEM
+# T cells: CD4, CD8
 # Macrophage: IM, LAM, PVM
 # Stromal: Myofibroblast, PCOLCE+ Fibroblast, MYOC+ Fibroblast, Preadipocyte
 rA <- list()
@@ -111,75 +110,6 @@ MakeCellObject <- function(subset, seurat_object) {
     return(cell.object)
 }
 
-###############################
-# Plot Results
-###############################
-Heatmap_fun <- function(seurat_object, subset = F, R, metadata = data_hatim, cluster, genes, tmp_dir = paste0("/data/p_koethe_lab/Atlas_AT/HATIM_Analysis/", date, "/Dialogue/", "All"),
-    units = "in", height = 10, width = 15, res = 300, filename, MCP_dim = 1) {
-    
-    # Pull out genes
-    clustergene <- gsub(" ", ".", cluster)
-    cluster.up <- R$MCPs[[paste0("MCP",MCP_dim)]][[paste0(clustergene, ".up")]]
-    cluster.down <- R$MCPs[[paste0("MCP",MCP_dim)]][[paste0(clustergene, ".down")]]
-    cluster_genes <- c(cluster.up, cluster.down)
-    
-    # Get Expression
-    if(subset) {
-        Idents(seurat_object) <- "Annotation"
-        seurat_object <- subset(seurat_object, idents = cluster)
-    }
-    Avg <- AverageExpression(seurat_object, group.by = "HATIMID")
-    Avg <- Avg[[1]]
-    
-    # Pull out cluster-specific genes
-    Avg <- Avg[rownames(Avg) %in% cluster_genes,]
-    
-    # Get rid of lowly expressed genes (likely ambient RNA contamination)
-    Exp <- data.frame(Expression = rowMeans(seurat_object@assays$RNA@data[rownames(seurat_object@assays$RNA@data) %in% cluster_genes,]))
-    Exp$Gene <- rownames(Exp)
-    Exp <- Exp[order(Exp$Expression, decreasing = T),]
-    Exp <- Exp[Exp$Expression > 0.15,]
-    Avg <- Avg[rownames(Avg) %in% Exp$Gene,]
-    names <- colnames(Avg)
-    names(names) <- NULL
-    
-    # Scale Expression
-    mat_scaled <- t(apply(Avg,1,scale))
-    colnames(mat_scaled) <- names
-
-    # Generate Metadata
-    DF <- data.frame(mat_scaled)
-    DF$MCP <- ifelse(rownames(DF) %in% cluster.up, "Up", "Down")
-    MCP_label <- DF$MCP
-    DF$MCP <- NULL
-    colnames(DF) <- names
-    tDF <- data.frame(t(DF))
-    tDF$HATIMID <- as.character(rownames(tDF))
-    merged <- left_join(tDF, metadata[,c("HATIMID", "StudyGroup", "BMI", "Age", "Sex", "FBG", "hba1c", "metformin")], by = "HATIMID")
-
-    # Heatmap
-    ha = HeatmapAnnotation(MP = MCP_label, col = list(MP = c("Up" = "red", "Down" = "blue")))
-    col_fun = colorRamp2(c(25,35,45), c("blue", "white", "red"))
-    col_fun2 = colorRamp2(c(25, 45, 65), c("#2D8A3D", "white", "#F49F2C"))
-    col_fun3 = colorRamp2(c(80, 110, 150), c("blue", "white", "red"))
-    col_fun4 = colorRamp2(c(5, 6, 8), c("#79FF33", "white", "#FF5733"))
-    row_ha = rowAnnotation(Group = merged$StudyGroup, Sex = merged$Sex, BMI = merged$BMI, Age = merged$Age, FBG = merged$FBG, HbA1c = merged$hba1c, col = list(Group = c("HIV+ non-diabetic"="green", "HIV+ prediabetic" = "blue", "HIV+ diabetic" = "orange", "HIV- diabetic" = "red"),
-    Sex = c("Male" = "dodgerblue", "Female" = "orange"), BMI = col_fun, Age = col_fun2, FBG = col_fun3, HbA1c = col_fun4))
-
-    index <- which(rownames(mat_scaled) %in% c(genes))
-    label <- rownames(mat_scaled)[index]
-    column_ha <- columnAnnotation(foo = anno_mark(at = index, labels = label, which = "column", side = "bottom"))
-    
-    png(file = paste(tmp_dir, filename, sep = "/"), res = res, width = width, height = height, units = units)
-    print({
-      Heatmap(t(mat_scaled), column_order = cluster_genes[cluster_genes %in% Exp$Gene], top_annotation = ha, right_annotation = row_ha, bottom_annotation = column_ha, show_column_names = F)
-    })
-    dev.off()
-    
-    plot <- Heatmap(t(mat_scaled), column_order = cluster_genes[cluster_genes %in% Exp$Gene], top_annotation = ha, right_annotation = row_ha, bottom_annotation = column_ha, show_column_names = F)
-    return(plot)
-}
-
 # 3. GENERATE CELL SPECIFIC OBJECTS_____________________________________________________
 ###############################
 # Generate List of Cell types HIV+ Only
@@ -213,12 +143,12 @@ Macrophage <- subset(Macrophage, cells = colnames(Macrophage)[Macrophage$HATIMID
 Macrophage[['HATIMID']] <- droplevels(Macrophage$HATIMID)
 Macrophage[['HATIMID']] <- as.character(Macrophage$HATIMID)
 
-Macrophage_list <- c("LAM", "IM", "PVM", "Mo-Mac")
+Macrophage_list <- c("LAM", "IM", "PVM", "Mo-Mac 1", "Mo-Mac 2")
 rA_Macrophage <- lapply(Macrophage_list, MakeCellObject, seurat_object = Macrophage)
 
 # Stromal
 Idents(Stromal) <- "Annotation"
-Idents(Stromal, cells = WhichCells(Stromal, idents = c("Mature Preadipocyte 1", "Mature Preadipocyte 2", "Early Preadipocyte", "ECM-Producing Early Preadipocyte"))) <- "Preadipocyte"
+Idents(Stromal, cells = WhichCells(Stromal, idents = c("Mature Preadipocyte", "Early Preadipocyte", "ECM-Producing Early Preadipocyte"))) <- "Preadipocyte"
 Idents(Stromal, cells = WhichCells(Stromal, idents = c("Adipose Progenitor Cell 1", "Adipose Progenitor Cell 2"))) <- "Progenitor"
 Stromal[['Annotation']] <- Idents(Stromal)
 Stromal[['HATIMID']] <- droplevels(Stromal$HATIMID)
@@ -233,25 +163,18 @@ rA_Stromal <- lapply(Stromal_list, MakeCellObject, seurat_object = Stromal)
 # Merge
 rA <- c(rA_CD4, rA_CD8, rA_Macrophage, rA_Stromal)
 
-tmp_dir <- paste0("/data/p_koethe_lab/Atlas_AT/HATIM_Analysis/", date, "/Dialogue/", "HIVneg")
+tmp_dir <- paste0("../HIVneg")
 dir.create(tmp_dir)
 saveRDS(rA, file = paste(tmp_dir, "rA_HIVneg.rds", sep = "/"))
 
+rm(Stromal)
+rm(Macrophage)
+rm(CD4)
+rm(CD8)
+
 # 4. Run Dialogue_____________________________________________________
-######################
+###############################
 # Run Dialogue All Cells
 ###############################
 R <- DIALOGUE.run(rA = rA, main = "HIVneg", k = 5, results.dir = tmp_dir, pheno = "GlucoseIntolerant", plot.flag = T, conf = c('cellQ', 'Sex', 'BMI', 'Age'))
 saveRDS(R, file = paste(tmp_dir, "Results_HIVneg.rds", sep = "/"))
-
-# 5. PLOT HEATMAP_____________________________________________________
-R <- readRDS(paste(tmp_dir, "Results_HIVneg.rds", sep = "/"))
-
-
-###############################
-# Generate Heatmap Overall
-###############################
-# CD4 MCP1
-plot <- Heatmap_fun(seurat_object = CD4, subset = F, metadata = data_hatim, cluster = "CD4", genes = c("CCR7", "LDHB", "LEF1", "LEPROTL1", "WNT7A", "TCF7", "SELL", "ALOX5AP", "CCL5", "CD36",
-"ANXA1", "ANXA2", "CCL4L2", "APOBEC3G", "CTSD", "COTL1", "FOS", "FABP4", "GBP5", "GZMK", "GZMA", "HOPX", "IFITM3", "IFNG", "IL32", "KLRG1", "LGALS3", "S100A4", "SH3BGRL3", "TNF", "VIM"), 
-tmp_dir = fig_dir, filename = "Figure8E.png", R = R, MCP_dim = 1)
